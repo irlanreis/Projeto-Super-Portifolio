64080511394b3d968fefa42a6e3060533ba10bff
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-28 14:47:33 -0400

    Feat: Requisto 01 Finished

--- projects/apps.py
+++ projects/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppPortifolioConfig(AppConfig):
+class ProjectsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "app_portifolio"
+    name = "projects"
