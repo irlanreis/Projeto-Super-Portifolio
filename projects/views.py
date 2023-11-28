8af717104228b42963162b0552ad6a69c32b3dbc
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2023-11-28 15:59:57 -0400

    Feat: Requisito 02

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ProfileSerializer
+from .models import Profile
 
-# Create your views here.
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrive(self, request, *args, **kwargs):
+        if self.request.method == "GET":
+            profile_id = self.kwargs["pk"]
+            profile = Profile.objects.get(id=profile_id)
+            context = {
+                "profile": profile,
+                "projects": profile.project_set.all(),
+                "certificates": profile.certificate_set.all(),
+            }
+
+            return render(request, "profile_detail.html", context)
+
+        return super().retrieve(request, *args, **kwargs)
--- super_portfolio/urls.py
+++ super_portfolio/urls.py
@@ -1,16 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
 
-router = routers.DefaultRouter()
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include("projects.urls")),
     path("admin/", admin.site.urls),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
